3f518c8c35974ca5d635e8931a85a240dbcf7b9d
Kauã Campos <168349958+makizs@users.noreply.github.com>
2025-03-09 11:17:50 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('#navbar');
+let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll ('section');
 let navLinks = document.querySelectorAll ('header nav a');
 
